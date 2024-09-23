de668088ece0e04e551cdb16f8fda2522ae05ac0
Abhayraj Singh Mandloi <139910814+Abhayraj11tech@users.noreply.github.com>
2024-09-23 16:57:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            obj.print_divisors(n);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static void print_divisors(int n) {
+        Set<Integer> set = new TreeSet<>();
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                set.add(i);
+                if(n/i != i) set.add(n/i);
+            }
+        }
+    for(int i : set){
+        System.out.print(i+" ");
+    }
+    }
+}
+
